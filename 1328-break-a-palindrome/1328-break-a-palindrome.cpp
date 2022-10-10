3cb13ad22a132f2acf2fefc53a47009b69cec75c
Ahmad Hamadneh <97923592+amjhamadneh@users.noreply.github.com>
2022-10-10 18:21:23 +0300

    Time: 0 ms (100.00%), Space: 6.2 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     string breakPalindrome(string s) {
-         if(s.size() <= 1) return "";
+        if(s.size() <= 1) return "";
         int index = -1;
         if(s.size() % 2)
-            index = (s.size() / 2);
-  for(int i =0; i< s.size(); ++i){
-    int temp = s[i];
-    temp--;
-    if((char)(temp) < s[i] && (char)(temp) >= 'a' && i != index){
-      s[i] = 'a';
-      return s;
-      }
-  }
-  for(int i =s.size() - 1; i>=0 ; --i){
+        index = (s.size() / 2);
+        for(int i =0; i< s.size(); ++i){
+            int temp = s[i];
+            temp--;
+            if((char)(temp) < s[i] && (char)(temp) >= 'a' && i != index){
+              s[i] = 'a';
+              return s;
+            }
+        }
+        for(int i =s.size() - 1; i>=0 ; --i){
 
-    int temp = s[i];
-    temp++;
-    if((char)(temp) > s[i] && (char)(temp) <= 'z' && i != index){
-      s[i]++;
-      return s;
-    }
-  }  
-  return "";
+            int temp = s[i];
+            temp++;
+            if((char)(temp) > s[i] && (char)(temp) <= 'z' && i != index){
+              s[i]++;
+              return s;
+            }
+        }  
+        return "";
     }
 };
